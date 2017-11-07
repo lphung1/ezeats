888fc51e5aaa5ca8b5ee73e43edd6d13ea844cf2
Loi Phung <lphung1@uncc.edu>
2017-11-07 16:59:00 +0000

    Added some comments, fixed 'index not found error'. View's must be left as 'index' because it is the default page for rails views.

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,2 +1,4 @@
+#model for drink contains table name 'drinks' with the following attributes
+#name:string -indicating name of drink ingredients:string - indicating ingredients contained in the drink
 class Drink < ActiveRecord::Base
 end
